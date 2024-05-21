08da0476a31f268cae8e673f8c7b24e437f6f2e2
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-05-21 03:11:48 +0300

    fix wrong color bug

--- MealsToGo/src/features/restaurants/screens/restaurants.screen.js
+++ MealsToGo/src/features/restaurants/screens/restaurants.screen.js
@@ -35,7 +35,7 @@ export const RestaurantsScreen = () => {
     <SafeArea>
       {isLoading && (
         <LoadingContainer>
-          <Loading size={50} animating={true} color={Colors.blue300} />
+          <Loading size={50} animating={true} color={colors.brand.primary} />
         </LoadingContainer>
       )}
       <SearchContainer>
