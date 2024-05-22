ae592f86f60a53e2ce17c04aa5209c577919f5eb
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-05-22 03:16:21 +0300

    auth test

--- MealsToGo/App.js
+++ MealsToGo/App.js
@@ -1,5 +1,5 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components/native";
 import * as firebase from "firebase";
 
@@ -17,16 +17,34 @@ import { LocationContextProvider } from "./src/services/location/location.contex
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCPFpUafdMm2ePXqzX92NKKHzJHowE_Qqc",
-  authDomain: "mealstogo-f881c.firebaseapp.com",
-  projectId: "mealstogo-f881c",
-  storageBucket: "mealstogo-f881c.appspot.com",
-  messagingSenderId: "50019520339",
-  appId: "1:50019520339:web:e88fe5fe3fea4650e5dba6",
+  apiKey: "AIzaSyBmKwzXIQ_7iufDh4U6GyU_4Wc-hyDEnf8",
+  authDomain: "mealstogo-b2612.firebaseapp.com",
+  projectId: "mealstogo-b2612",
+  storageBucket: "mealstogo-b2612.appspot.com",
+  messagingSenderId: "158582890553",
+  appId: "1:158582890553:web:2ef50439fcd80f2ede2de8",
 };
-firebase.initializeApp(firebaseConfig);
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword("mo@binni.io", "test123")
+        .then((user) => {
+          setIsAuthenticated(true);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }, 2000);
+  }, []);
+
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
@@ -38,6 +56,8 @@ export default function App() {
   if (!oswaldLoaded || !latoLoaded) {
     return null;
   }
+
+  if (!isAuthenticated) return null;
 
   return (
     <>
