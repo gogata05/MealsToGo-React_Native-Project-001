16aea0f3d448dc9f931d5334d7e77cf5bfb4366e
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-05-24 04:50:26 +0300

    add stripe "Publishable Key" token - stripe API key

--- MealsToGo/src/features/checkout/components/credit-card.component.js
+++ MealsToGo/src/features/checkout/components/credit-card.component.js
@@ -1,11 +1,25 @@
 import React from "react";
 import { LiteCreditCardInput } from "react-native-credit-card-input";
+import createStripe from "stripe-client";
+
+const stripe = createStripe(
+  "pk_test_51PHWLfHTTSVORgAjtEN7Z3pQZgEL12ls5pMPJ1zBBJDB6lVN1SEnmzNQNxUQOHx6JGRc9Kcok0Ues6oj0glyk9WE00Lzi7oDNX"
+);
 
 export const CreditCardInput = () => {
-  const onChange = (formData) => {
+  const onChange = async (formData) => {
     const { values, status } = formData;
     const isIncomplete = Object.values(status).includes("incomplete");
     console.log(isIncomplete);
+    const card = {
+      number: "4242424242424242",
+      exp_month: "02",
+      exp_year: "26",
+      cvc: "244",
+      name: "Mo",
+    };
+    const info = await stripe.createToken({ card });
+    console.log(info);
   };
   return <LiteCreditCardInput onChange={onChange} />;
 };
